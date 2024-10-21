a1c17d47fa8cc81f21ee2edcba32ac55f744b7b7
Jose Estrada <jestrada@efrouting.com>
2024-10-21 13:54:03 -0500

    manga project: interfaz para infomación de los mangas mas populares de la actualidad

---
+++ js/series.js
@@ -0,0 +1,201 @@
+const slider = [
+    {   "id": 1,
+        "title": "El hijo bastardo del marqués era el emperador",
+        "description": "«¡Un emperador que murió injustamente se convierte en un desgraciado hijo bastardo!» Ian, un mago del Imperio de Bariel, se convirtió en emperador a una edad temprana, pero fue encarcelado debido a la rebelión de su sobrino. Sin embargo, fue salvado por el mago Naum utilizando magia espacio-temporal prohibida. Esta magia lleva al emperador Ian 100 años al pasado, donde posee al Bastardo Ian, el hijo ilegítimo de la familia Margrave que se extinguió hace 100 años. El bastardo Ian está a punto de ser tomado como rehén por los bárbaros. ¡¡¡Ian está a punto de verse envuelto en una sangrienta conspiración!!!",
+        "banner": "https://dashboard.zonaolympus.com/storage/sliders/11/banner-emperador-xl.webp",
+        "banner_srcset": "https://dashboard.zonaolympus.com/storage/sliders/11/banner-emperador-md.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/11/banner-emperador-xl.webp 1536w",
+        "banner_mini_srcset": "https://dashboard.zonaolympus.com/storage/sliders/11/banner-emperador-sm.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/11/banner-emperador-lg.webp 1536w",
+        "url": "https://zonaolympus.com/series/comic-el-hijo-bastardo-del-marques-era-el-emperador"
+    },
+    {
+        "id": 2,
+        "title": "Solo Leveling: Ragnarok",
+        "description": "La existencia de la Tierra se ve amenazada una vez más cuando los Itarim, los dioses de otros universos, intentan llenar el vacío dejado tras la muerte del Ser Absoluto. Sung Jinwoo no tiene más remedio que enviar a Beru, el rey hormiga de las sombras, para despertar los poderes de su hijo e iniciarle en el viaje que una vez emprendió. Suho deberá conquistar la mazmorra de las sombras y ganarse su lugar en el mundo de los cazadores mientras navega por un nuevo mundo contra un nuevo mal que busca tragarse el mundo entero.",
+        "banner": "https://dashboard.zonaolympus.com/storage/sliders/16/banner-emperador-xl.webp",
+        "banner_srcset": "https://dashboard.zonaolympus.com/storage/sliders/16/banner-emperador-md.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/16/banner-emperador-xl.webp 1536w",
+        "banner_mini_srcset": "https://dashboard.zonaolympus.com/storage/sliders/16/banner-emperador-sm.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/16/banner-emperador-lg.webp 1536w",
+        "url": "https://zonaolympus.com/series/comic-solo-leveling-ragnarok"
+    },
+    {
+        "id": 3,
+        "title": "El hijo menor del maestro de la espada",
+        "description": "Jin Runcandel era el hijo menor de Runcandel, la familia de espadachines más prestigiosa del país... Y el mayor fracaso en la historia de Runcandel. A él, que fue expulsado miserablemente y llegó a un final sin sentido, se le dio otra oportunidad. \"¿Cómo quieres usar este poder?\" \"Quiero usarlo para mí\". Recuerdos de su vida pasada, un talento abrumador y un contrato con Dios... Los preparativos para convertirse en el más grande están completos...",
+        "banner": "https://dashboard.zonaolympus.com/storage/sliders/1/hijo_menor_olympus-xl.webp",
+        "banner_srcset": "https://dashboard.zonaolympus.com/storage/sliders/1/hijo_menor_olympus-md.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/1/hijo_menor_olympus-xl.webp 1536w",
+        "banner_mini_srcset": "https://dashboard.zonaolympus.com/storage/sliders/1/hijo_menor_olympus-sm.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/1/hijo_menor_olympus-lg.webp 1536w",
+        "url": "https://zonaolympus.com/series/comic-el-hijo-de-la-espada13424"
+    },
+    {
+        "id": 4,
+        "title": "Regreso del ranker de clase SSS",
+        "description": "Lokan era el \"Rey de la Violencia\" que reinaba como el más fuerte en el juego de realidad virtual \"El Señor\". Por desgracia, la orden de asesinato emitida por un gremio enemigo hizo que perdiera todo. Para su sorpresa, la siguiente vez que despertó, ¡había regresado a tres años atrás! \"Malditos mocosos, esperen. Los voy a devorar a todos\". Comienza una nueva historia en la que Rokan, que volvió en el tiempo, vuelve a subir a la cima.",
+        "banner": "https://dashboard.zonaolympus.com/storage/sliders/17/ranker-xl.webp",
+        "banner_srcset": "https://dashboard.zonaolympus.com/storage/sliders/17/ranker-md.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/17/ranker-xl.webp 1536w",
+        "banner_mini_srcset": "https://dashboard.zonaolympus.com/storage/sliders/17/ranker-sm.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/17/ranker-lg.webp 1536w",
+        "url": "https://leerolymp.com/series/comic-regreso-del-ranker-de-clase-ss13424"
+    },
+    {
+        "id": 5,
+        "title": "Inscripcion de mercenarios (El Asesino Yu Ijin)",
+        "description": "Yu Ijin fue el único sobreviviente de un accidente de avión cuando era pequeño. Después de convertirse en mercenario para sobrevivir durante 10 años, regresa con su familia a su ciudad natal.",
+        "banner": "https://dashboard.zonaolympus.com/storage/sliders/14/banermercenario-xl.webp",
+        "banner_srcset": "https://dashboard.zonaolympus.com/storage/sliders/14/banermercenario-md.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/14/banermercenario-xl.webp 1536w",
+        "banner_mini_srcset": "https://dashboard.zonaolympus.com/storage/sliders/14/banermercenario-sm.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/14/banermercenario-lg.webp 1536w",
+        "url": "https://leerolymp.com/series/comic-inscripcion-de-mercenarios-el-asesino-yu-ijin"
+    },
+    {
+        "id": 6,
+        "title": "Guía de supervivencia del extra de la academia",
+        "description": "Soy un extra villano de tercera en mi juego favorito. No tengo ambiciones y sólo quiero vivir una vida tranquila. Pero vaya, este mundo es un lugar difícil para sobrevivir. Sobreviviré hasta el final de esta historia, de una forma que el protagonista no puede. Sobreviviré a la manera de un villano de tercera.",
+        "banner": "https://dashboard.zonaolympus.com/storage/sliders/12/banner-guia1-xl.webp",
+        "banner_srcset": "https://dashboard.zonaolympus.com/storage/sliders/12/banner-guia1-md.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/12/banner-guia1-xl.webp 1536w",
+        "banner_mini_srcset": "https://dashboard.zonaolympus.com/storage/sliders/12/banner-guia1-sm.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/12/banner-guia1-lg.webp 1536w",
+        "url": "https://leerolymp.com/series/comic-el-extra-protagonista-1n"
+    },
+    {
+
+        "id": 7,
+        "title": "Como vivir como el principe enemigo",
+        "description": "Podía oír el zumbido de la vida en mis oídos. Era difícil respirar.... ... Levanté la mirada y miré a lo lejos. Sin embargo, mi visión borrosa no podía ver nada. La luz empezó a desvanecerse. Ese fue mi último recuerdo. Cuando volví a abrir los ojos, habían pasado diez años desde aquel último recuerdo. Y de todas las cosas en las que podría haberme convertido, fue en el país enemigo que llevó a Secretia a la ruina: me convertí en el tercer príncipe de Kyris, Calian. ..... Bern era el orgulloso príncipe heredero de Secretia que luchó y murió en la guerra contra Kyris. Pero despertó 10 años en el pasado - como el tímido Tercer Príncipe de Kyris, Calian.",
+        "banner": "https://dashboard.zonaolympus.com/storage/sliders/13/baner-principe-xl.webp",
+        "banner_srcset": "https://dashboard.zonaolympus.com/storage/sliders/13/baner-principe-md.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/13/baner-principe-xl.webp 1536w",
+        "banner_mini_srcset": "https://dashboard.zonaolympus.com/storage/sliders/13/baner-principe-sm.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/13/baner-principe-lg.webp 1536w",
+        "url": "https://leerolymp.com/series/comic-como-vivir-como-el-principe-enemigo5324"
+    },
+    {
+        "id": 8,
+        "title": "Jugador que regresó 10.000 años después",
+        "description": "Un día, de la nada, cae en el infierno. Lo único que lleva consigo es su deseo de vivir y sus poderes depredadores. Del infierno milenario al noveno milenario, ha devorado decenas y cientos de miles de demonios, e incluso los siete archiduques se inclinaron ante él. “¿Por qué quieres volver? Su Alteza ya tiene todo en el infierno”. “Todo, una mierda”. ¡No hay nada que comer o disfrutar aquí! ¡El infierno no es más que una tierra desolada llena de terribles demonios! “Volveré”. Después de diez mil largos años, finalmente regresa a la Tierra.",
+        "banner": "https://dashboard.zonaolympus.com/storage/sliders/4/fundo_pry-xl.webp",
+        "banner_srcset": "https://dashboard.zonaolympus.com/storage/sliders/4/fundo_pry-md.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/4/fundo_pry-xl.webp 1536w",
+        "banner_mini_srcset": "https://dashboard.zonaolympus.com/storage/sliders/4/fundo_pry-sm.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/4/fundo_pry-lg.webp 1536w",
+        "url": "https://leerolymp.com/series/comic-jugador-que-regreso-10000-anos-despues13424"
+    },
+    {
+        "id": 9,
+        "title": "Nueva Vida del Jugador",
+        "description": "[¡Del estudio que te trajo ‘Maldita Reencarnación’ y ‘Mi Vida Escolar Fingiendo Ser una Persona Inútil’!]\n\nDespués de perder a su familia y cerrar su corazón, sólo quería matar a los monstruos que detestaba.\n\nRetrocedí antes de que mi vida llegara a su fin en la parte más profunda de la <Mazmorra del abismo> que era imposible de asaltar para la humanidad.\n\nYa que he renacido como un bebé, hagamos que esta vida sea diferente.\nHaré cualquier cosa por el bien de mi felicidad.\n\nMataré para vivir, y haré todo lo posible para sobrevivir.\nIncluso si tengo que recorrer un camino espinoso yo solo sin que nadie me reconozca.\n\nEn esta vida, definitivamente-",
+        "banner": "https://dashboard.zonaolympus.com/storage/sliders/5/re_life-xl.webp",
+        "banner_srcset": "https://dashboard.zonaolympus.com/storage/sliders/5/re_life-md.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/5/re_life-xl.webp 1536w",
+        "banner_mini_srcset": "https://dashboard.zonaolympus.com/storage/sliders/5/re_life-sm.webp 768w, https://dashboard.zonaolympus.com/storage/sliders/5/re_life-lg.webp 1536w",
+        "url": "https://visorolym.com/series/comic-nueva-vida-del-jugador7777"
+    },
+    {
+        "title": "Resurgimiento de la Sombra",
+        "slug": "resurgimiento-de-la-sombra",
+        "id": 10,
+        "banner": "https://dashboard.zonaolympus.com/storage/comics/covers/1304/22-lg.webp",
+        "cover_srcset": "https://dashboard.zonaolympus.com/storage/comics/covers/1304/22-lg.webp 768w, https://dashboard.zonaolympus.com/storage/comics/covers/1304/22-xl.webp 1536w",
+        "status": null,
+        "type": "comic",
+        "description": "La oscuridad regresa a la tierra y un grupo de héroes, cada uno con sus propias habilidades y pasados, debe unirse para enfrentar la inminente amenaza que se cierne sobre su mundo. A medida que se forman alianzas inesperadas, se revelan secretos ocultos y traiciones dentro del propio grupo. Cada héroe, en su búsqueda de redención, debe enfrentar sus demonios internos y descubrir su verdadero poder para poder superar el mal que los acecha. La lucha no solo es contra las sombras externas, sino también contra las sombras que habitan en sus corazones."
+    },
+    {
+        "title": "La luz de Arad: Aquel que camina primero",
+        "slug": "la-luz-de-arad-aquel-que-camina-primero",
+        "id": 11,
+        "banner": "https://dashboard.zonaolympus.com/storage/comics/covers/1394/perlas-lg.webp",
+        "cover_srcset": "https://dashboard.zonaolympus.com/storage/comics/covers/1394/perlas-lg.webp 768w, https://dashboard.zonaolympus.com/storage/comics/covers/1394/perlas-xl.webp 1536w",
+        "status": null,
+        "type": "comic",
+        "description": "En un mundo lleno de magia, misterios y antiguas leyendas, un joven aventurero emprende un viaje épico para desentrañar la verdad detrás de su linaje y el destino que le aguarda. A medida que avanza, se encuentra con criaturas fantásticas y aliados inesperados que le ayudarán en su búsqueda. Sin embargo, también deberá enfrentar enemigos poderosos que desean mantener los secretos del pasado ocultos. Cada paso que da lo lleva más cerca de descubrir quién es realmente y qué papel juega en el gran esquema del universo."
+    },
+    {
+        "title": "Crónicas de la facción demoníaca (Chun Hajin, el asesino más fuerte)",
+        "slug": "cronicas-de-la-faccion-demoniaca-chun-hajin-el-asesino-mas-fuerte",
+        "id": 12,
+        "banner": "https://dashboard.zonaolympus.com/storage/comics/covers/653/Photoauto_scaleLevel3width_1000-lg.webp",
+        "cover_srcset": "https://dashboard.zonaolympus.com/storage/comics/covers/653/Photoauto_scaleLevel3width_1000-lg.webp 768w, https://dashboard.zonaolympus.com/storage/comics/covers/653/Photoauto_scaleLevel3width_1000-xl.webp 1536w",
+        "status": null,
+        "type": "comic",
+        "description": "Chun Hajin, un legendario asesino con un pasado marcado por la traición y la violencia, debe enfrentarse a su oscuro legado mientras navega en un mundo repleto de facciones demoníacas que buscan su destrucción. En su camino, se encontrará con antiguos aliados y enemigos, desenterrando secretos que podrían cambiar el rumbo de su vida. Cada decisión que tome podría determinar no solo su destino, sino también el equilibrio entre el bien y el mal en un mundo lleno de caos."
+    },
+    {
+        "title": "La Leyenda De La Estrella General (Continuación)",
+        "slug": "la-leyenda-de-la-estrella-general-continuacion",
+        "id": 13,
+        "banner": "https://dashboard.zonaolympus.com/storage/comics/covers/1250/420-lg.webp",
+        "cover_srcset": "https://dashboard.zonaolympus.com/storage/comics/covers/1250/420-lg.webp 768w, https://dashboard.zonaolympus.com/storage/comics/covers/1250/420-xl.webp 1536w",
+        "status": null,
+        "type": "comic",
+        "description": "Las aventuras del General Estelar continúan mientras se enfrenta a nuevos enemigos y descubre verdades ocultas sobre el universo. En su viaje, deberá unir fuerzas con viejos amigos y hacer sacrificios para proteger lo que más ama. A medida que se desvelan secretos del pasado, el General debe decidir si el poder que ha obtenido es una bendición o una maldición. Su lucha no solo definirá su destino, sino que también cambiará el curso de la historia de su mundo."
+    },
+    {
+        "title": "El Villano Del Destino",
+        "slug": "el-villano-del-destino77",
+        "id": 14,
+        "banner": "https://dashboard.zonaolympus.com/storage/comics/covers/562/152---thumbnail_IMAG21_d3905287-424c-4dd7-96a0-733911654a31-lg.webp",
+        "cover_srcset": "https://dashboard.zonaolympus.com/storage/comics/covers/562/152---thumbnail_IMAG21_d3905287-424c-4dd7-96a0-733911654a31-lg.webp 768w, https://dashboard.zonaolympus.com/storage/comics/covers/562/152---thumbnail_IMAG21_d3905287-424c-4dd7-96a0-733911654a31-xl.webp 1536w",
+        "status": null,
+        "type": "comic",
+        "description": "Un villano inesperado se convierte en la clave para la salvación del mundo. A medida que sus planes se desarrollan, la línea entre el bien y el mal se difumina. Este villano, lejos de ser el típico antagonista, revela una profundidad emocional y motivaciones que desafían las nociones tradicionales de la moralidad. A través de giros sorprendentes y revelaciones impactantes, los personajes se verán obligados a cuestionar sus propios ideales y lo que realmente significa ser un héroe en tiempos de crisis."
+    },
+    {
+        "title": "Hijos de la Runa",
+        "slug": "hijos-de-la-runa",
+        "id": 15,
+        "banner": "https://dashboard.zonaolympus.com/storage/comics/covers/1364/resource_1-vertCUnetauto_scaleLevel3width_2000-lg.webp",
+        "cover_srcset": "https://dashboard.zonaolympus.com/storage/comics/covers/1364/resource_1-vertCUnetauto_scaleLevel3width_2000-lg.webp 768w, https://dashboard.zonaolympus.com/storage/comics/covers/1364/resource_1-vertCUnetauto_scaleLevel3width_2000-xl.webp 1536w",
+        "status": null,
+        "type": "comic",
+        "description": "Un grupo de jóvenes guerreros se embarca en una misión para recuperar las runas perdidas que pueden cambiar el destino de su mundo para siempre. En su travesía, se enfrentan a pruebas que pondrán a prueba no solo su fuerza, sino también su lealtad y amistad. Cada runa que encuentran revela fragmentos de un antiguo poder que desatará tanto grandeza como destrucción. A medida que avanzan, deben decidir si usar ese poder para bien o si sucumbir a la tentación de usarlo para sus propios fines."
+    }
+
+   
+]
+
+
+
+const renderSliderItems = (items) => {
+    const container = document.querySelector('#items');
+
+    container.innerHTML = '';
+    const sliderContainer = document.createElement('div');
+    sliderContainer.className = 'items-container';
+    
+    items.map((item) => {
+        const sliderItem = document.createElement('div');
+        sliderItem.className = 'slider-item';
+        const overlayDiv = document.createElement('div');
+        overlayDiv.className = 'overlay';
+        sliderItem.style.backgroundImage = `url(${item.banner})`;
+
+        const sliderTitle = document.createElement('h2');
+        sliderTitle.textContent = item.title;
+        overlayDiv.appendChild(sliderTitle);
+        sliderItem.appendChild(overlayDiv);
+        sliderContainer.appendChild(sliderItem);
+
+        sliderItem.onclick = function () {
+            openDetailPage(item.id);
+        };
+    });
+
+    container.appendChild(sliderContainer);
+}
+
+const openDetailPage = (itemId) => {
+    window.location.href = `detail.html?item=${itemId}`;
+}
+
+const searchSlider = () => {
+    const searchValue = document.getElementById('search').value.toLowerCase();
+    const sanitizedSearchValue = searchValue
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+
+    console.log(sanitizedSearchValue);
+
+    const filteredItems = slider.filter(item => 
+        item.title.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().includes(sanitizedSearchValue)
+    );
+
+    renderSliderItems(filteredItems);
+}
+
+
+
+renderSliderItems(slider);
+
+
